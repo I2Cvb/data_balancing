b694fa3b38ca522189013efe6789dcfe599c57d6
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2015-06-01 01:14:12 +0200

    convert the adult dataset

--- src/data_conversion/conversion.py
+++ src/data_conversion/conversion.py
@@ -6,19 +6,30 @@ from scipy.io import loadmat
 
 # Abalone dataset - Convert the ring = 19 to class 1 and the other to class 0
 
-filename = '../../data/raw/mldata/uci-20070111-abalone.mat'
-matfile = loadmat(filename)
+# filename = '../../data/raw/mldata/uci-20070111-abalone.mat'
+# matfile = loadmat(filename)
 
-sex_array = np.zeros(np.ravel(matfile['int1']).shape[0])
-sex_array[np.nonzero(np.ravel(matfile['Sex']) == 'M')] = 0
-sex_array[np.nonzero(np.ravel(matfile['Sex']) == 'F')] = 1
-sex_array[np.nonzero(np.ravel(matfile['Sex']) == 'I')] = 2
+# sex_array = np.zeros(np.ravel(matfile['int1']).shape[0])
+# sex_array[np.nonzero(np.ravel(matfile['Sex']) == 'M')] = 0
+# sex_array[np.nonzero(np.ravel(matfile['Sex']) == 'F')] = 1
+# sex_array[np.nonzero(np.ravel(matfile['Sex']) == 'I')] = 2
 
-data = np.zeros((np.ravel(matfile['int1']).shape[0], 8))
-data[:, 0] = sex_array
-data[:, 1::] = matfile['double0'].T
+# data = np.zeros((np.ravel(matfile['int1']).shape[0], 8))
+# data[:, 0] = sex_array
+# data[:, 1::] = matfile['double0'].T
 
-label = np.zeros((np.ravel(matfile['int1']).shape[0], ), dtype=(int))
-label[np.nonzero(np.ravel(matfile['int1']) == 19)] = 1
+# label = np.zeros((np.ravel(matfile['int1']).shape[0], ), dtype=(int))
+# label[np.nonzero(np.ravel(matfile['int1']) == 19)] = 1
 
-np.savez('../../data/raw/mldata/uci-20070111-abalone.npz', data=data, label=label)
+# np.savez('../../data/raw/mldata/uci-20070111-abalone.npz', data=data, label=label)
+
+# Adult dataset
+
+# filename = '../../data/raw/mldata/adult'
+
+# input = np.loadtxt(filename, delimiter = ',', usecols = (0, 2, 4, 10, 11, 12, 14))
+
+# data = input[:, :-1]
+# label = input[:, -1]
+
+# np.savez('../../data/raw/mldata/uci-adult.npz', data=data, label=label)
