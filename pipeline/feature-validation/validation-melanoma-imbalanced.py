f09e0f1afd6f0f6a7bf99daa4359805d1142a6d5
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-01-19 21:08:23 +0100

    Add the results with Naive Bayes and finished the validation

--- pipeline/feature-validation/validation-melanoma-imbalanced.py
+++ pipeline/feature-validation/validation-melanoma-imbalanced.py
@@ -64,137 +64,137 @@ bal_arr = ['No balancing',
 ############################## LOADING DATA ##############################
 
 # Load the file of interest
-path_to_results = '../../results/data/melanoma/random-forest/melanoma_imbalanced_80_20_100_2.npz'
+path_to_results = '../../results/data/melanoma/naive-bayes/melanoma_imbalanced_80_20_100_2.npz'
 file_results = np.load(path_to_results)
 
-# ############################## ROC ANALYSIS ##############################
+############################## ROC ANALYSIS ##############################
 
-# # build an roc_auc object for each curve
-# kind_interp = 'linear'
-# roc_by_config = []
-# roc_fitted_by_config = []
-# for config in file_results['rocs']:
-#     # for each cross validation
-#     roc_by_fold = []
-#     roc_fitted_by_fold = []
-#     for k in config:
-#         roc_by_fold.append(roc_auc(k[0], k[1], k[2], k[3]))
-#         # Declare the x for the linspace
-#         roc_fitted_by_fold.append(interp1d(k[0], k[1], kind=kind_interp))
+# build an roc_auc object for each curve
+kind_interp = 'linear'
+roc_by_config = []
+roc_fitted_by_config = []
+for config in file_results['rocs']:
+    # for each cross validation
+    roc_by_fold = []
+    roc_fitted_by_fold = []
+    for k in config:
+        roc_by_fold.append(roc_auc(k[0], k[1], k[2], k[3]))
+        # Declare the x for the linspace
+        roc_fitted_by_fold.append(interp1d(k[0], k[1], kind=kind_interp))
 
-#     roc_by_config.append(roc_by_fold)
-#     roc_fitted_by_config.append(roc_fitted_by_fold)
+    roc_by_config.append(roc_by_fold)
+    roc_fitted_by_config.append(roc_fitted_by_fold)
 
-# ############################## PLOTTING DATA ##############################
+############################## PLOTTING DATA ##############################
 
-# plt.figure()
-# # Compute a single ROC using the mean and the standard deviation with interpolation in the middle
-# for cof in range(len(roc_by_config)):
-#     tpr_arr = []
-#     for roc in roc_fitted_by_config[cof]:
-#         # create the fpr in order to compute the tpr
-#         fpr = np.linspace(0., 1., 100, endpoint=True)
-#         tpr_arr.append(roc(fpr))
+plt.figure()
+# Compute a single ROC using the mean and the standard deviation with interpolation in the middle
+for cof in range(len(roc_by_config)):
+    tpr_arr = []
+    for roc in roc_fitted_by_config[cof]:
+        # create the fpr in order to compute the tpr
+        fpr = np.linspace(0., 1., 100, endpoint=True)
+        tpr_arr.append(roc(fpr))
 
-#     # Get the mean auc 
-#     auc_arr = []
-#     for roc in roc_by_config[cof]:
-#         auc_arr.append(roc.auc)
-#     auc_mean = np.mean(auc_arr, axis=0)
-#     auc_std = np.std(auc_arr, axis=0)
+    # Get the mean auc 
+    auc_arr = []
+    for roc in roc_by_config[cof]:
+        auc_arr.append(roc.auc)
+    auc_mean = np.mean(auc_arr, axis=0)
+    auc_std = np.std(auc_arr, axis=0)
 
-#     # Plot the mean curve
-#     mean_tpr = np.mean(tpr_arr, axis=0)
-#     std_tpr = np.std(tpr_arr, axis=0)
-#     fpr = np.linspace(0., 1., 100, endpoint=True)
-#     plt.plot(fpr, mean_tpr, lw=2, color=tableau20[cof],
-#              label=bal_arr[cof]+' - AUC={:.2f} +- {:.2f}'.format(auc_mean, auc_std))
-#     #plt.fill_between(fpr, mean_tpr+std_tpr, mean_tpr-std_tpr, facecolor=tableau20[cof], alpha=0.2)
+    # Plot the mean curve
+    mean_tpr = np.mean(tpr_arr, axis=0)
+    std_tpr = np.std(tpr_arr, axis=0)
+    fpr = np.linspace(0., 1., 100, endpoint=True)
+    plt.plot(fpr, mean_tpr, lw=2, color=tableau20[cof],
+             label=bal_arr[cof]+' - AUC={:.2f} +- {:.2f}'.format(auc_mean, auc_std))
+    #plt.fill_between(fpr, mean_tpr+std_tpr, mean_tpr-std_tpr, facecolor=tableau20[cof], alpha=0.2)
 
-# # Plot the legend on the left of the figure
-# lgd = plt.legend(loc='center left', bbox_to_anchor=(1.05, .5), prop={'size':7})
+# Plot the legend on the left of the figure
+lgd = plt.legend(loc='center left', bbox_to_anchor=(1.05, .5), prop={'size':7})
 
-# # Set the limit
-# plt.xlim(0, 1)
-# plt.ylim(0, 1)
+# Set the limit
+plt.xlim(0, 1)
+plt.ylim(0, 1)
 
-# # Put some label
-# plt.xlabel('False Positive Rate')
-# plt.ylabel('True Positive Rate')
+# Put some label
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
 
-# plt.show()
+plt.show()
 
 
-# ############################## PRECISION - RECALL ANALYSIS ##############################
+############################## PRECISION - RECALL ANALYSIS ##############################
 
-# # build an roc_auc object for each curve
-# kind_interp = 'linear'
-# pr_by_config = []
-# pr_fitted_by_config = []
-# for config_predl, config_predp, config_gt in zip(file_results['pred_labels'], 
-#                                                  file_results['pred_probs'], 
-#                                                  file_results['gt_labels']):
-#     # for each cross validation
-#     pr_by_fold = []
-#     pr_fitted_by_fold = []
+# build an roc_auc object for each curve
+kind_interp = 'linear'
+pr_by_config = []
+pr_fitted_by_config = []
+for config_predl, config_predp, config_gt in zip(file_results['pred_labels'], 
+                                                 file_results['pred_probs'], 
+                                                 file_results['gt_labels']):
+    # for each cross validation
+    pr_by_fold = []
+    pr_fitted_by_fold = []
 
-#     # get the gt and pred for a single cv
-#     for cv_predl, cv_predp, cv_gt in zip(config_predl, 
-#                                          config_predp,
-#                                          config_gt):
-#         # Compute the current recall precision curve
-#         pr_curve = precision_recall_curve(cv_gt, cv_predp[:, 1])
-#         # Insert the down limit
-#         print pr_curve[0].shape
-#         pr1 = np.concatenate(([0.], pr_curve[0]))
-#         pr2 = np.concatenate(([1.], pr_curve[1]))
-#         pr_curve = (pr1, pr2)
-#         print pr_curve[0].shape
-#         pr_by_fold.append(pr_curve)
-#         # Declare the x for the linspace
-#         pr_fitted_by_fold.append(interp1d(pr_curve[0], pr_curve[1], kind=kind_interp))
+    # get the gt and pred for a single cv
+    for cv_predl, cv_predp, cv_gt in zip(config_predl, 
+                                         config_predp,
+                                         config_gt):
+        # Compute the current recall precision curve
+        pr_curve = precision_recall_curve(cv_gt, cv_predp[:, 1])
+        # Insert the down limit
+        print pr_curve[0].shape
+        pr1 = np.concatenate(([0.], pr_curve[0]))
+        pr2 = np.concatenate(([1.], pr_curve[1]))
+        pr_curve = (pr1, pr2)
+        print pr_curve[0].shape
+        pr_by_fold.append(pr_curve)
+        # Declare the x for the linspace
+        pr_fitted_by_fold.append(interp1d(pr_curve[0], pr_curve[1], kind=kind_interp))
 
-#     pr_by_config.append(pr_by_fold)
-#     pr_fitted_by_config.append(pr_fitted_by_fold)
+    pr_by_config.append(pr_by_fold)
+    pr_fitted_by_config.append(pr_fitted_by_fold)
 
-# ############################## PLOTTING DATA ##############################
+############################## PLOTTING DATA ##############################
 
-# plt.figure()
-# # Compute a single ROC using the mean and the standard deviation with interpolation in the middle
-# for cof in range(len(pr_by_config)):
-#     prec_arr = []
-#     for pr in pr_fitted_by_config[cof]:
-#         # create the fpr in order to compute the tpr
-#         rec = np.linspace(0., 1., 40, endpoint=True)
-#         prec_arr.append(pr(rec))
+plt.figure()
+# Compute a single ROC using the mean and the standard deviation with interpolation in the middle
+for cof in range(len(pr_by_config)):
+    prec_arr = []
+    for pr in pr_fitted_by_config[cof]:
+        # create the fpr in order to compute the tpr
+        rec = np.linspace(0., 1., 40, endpoint=True)
+        prec_arr.append(pr(rec))
 
-#     # # Get the mean auc 
-#     # auc_arr = []
-#     # for roc in roc_by_config[cof]:
-#     #     auc_arr.append(roc.auc)
-#     # auc_mean = np.mean(auc_arr, axis=0)
-#     # auc_std = np.std(auc_arr, axis=0)
+    # # Get the mean auc 
+    # auc_arr = []
+    # for roc in roc_by_config[cof]:
+    #     auc_arr.append(roc.auc)
+    # auc_mean = np.mean(auc_arr, axis=0)
+    # auc_std = np.std(auc_arr, axis=0)
 
-#     # Plot the mean curve
-#     mean_prec = np.mean(prec_arr, axis=0)
-#     std_prec = np.std(prec_arr, axis=0)
-#     rec = np.linspace(0, 1., 40, endpoint=True)
-#     plt.plot(rec, mean_prec, lw=2, color=tableau20[cof],
-#              label=bal_arr[cof])#+' - AUC={:.2f} +- {:.2f}'.format(auc_mean, auc_std))
-#     #plt.fill_between(rec, mean_prec+std_prec, mean_prec-std_prec, facecolor=tableau20[cof], alpha=0.2)
+    # Plot the mean curve
+    mean_prec = np.mean(prec_arr, axis=0)
+    std_prec = np.std(prec_arr, axis=0)
+    rec = np.linspace(0, 1., 40, endpoint=True)
+    plt.plot(rec, mean_prec, lw=2, color=tableau20[cof],
+             label=bal_arr[cof])#+' - AUC={:.2f} +- {:.2f}'.format(auc_mean, auc_std))
+    #plt.fill_between(rec, mean_prec+std_prec, mean_prec-std_prec, facecolor=tableau20[cof], alpha=0.2)
 
-# # Plot the legend on the left of the figure
-# lgd = plt.legend(loc='center left', bbox_to_anchor=(1.05, .5), prop={'size':7})
+# Plot the legend on the left of the figure
+lgd = plt.legend(loc='center left', bbox_to_anchor=(1.05, .5), prop={'size':7})
 
-# # Set the limit
-# plt.xlim(0, 1)
-# plt.ylim(0, 1)
+# Set the limit
+plt.xlim(0, 1)
+plt.ylim(0, 1)
 
-# # Put some label
-# plt.xlabel('Recall')
-# plt.ylabel('Precision')
+# Put some label
+plt.xlabel('Recall')
+plt.ylabel('Precision')
 
-# plt.show()
+plt.show()
 
 ############################## COMPUTE THE DIFFERENT METRICS ##############################
 
@@ -203,10 +203,24 @@ file_results = np.load(path_to_results)
 ### all the mean metrics for each configuration
 mean_sens_config = []
 mean_spec_config = []
+mean_prec_config = []
+mean_npv_config = []
+mean_gmean_config = []
+mean_acc_config = []
+mean_f1sc_config = []
+mean_mcc_config = []
+mean_iba_config = []
 
 ### all the std metrics for each configuration
 std_sens_config = []
 std_spec_config = []
+std_prec_config = []
+std_npv_config = []
+std_gmean_config = []
+std_acc_config = []
+std_f1sc_config = []
+std_mcc_config = []
+std_iba_config = []
 
 for config_predl, config_predp, config_gt in zip(file_results['pred_labels'], 
                                                  file_results['pred_probs'], 
@@ -214,19 +228,57 @@ for config_predl, config_predp, config_gt in zip(file_results['pred_labels'],
     # for each cross validation
     cv_sens = []
     cv_spec = []
+    cv_prec = []
+    cv_npv = []
+    cv_gmean = []
+    cv_acc = []
+    cv_f1sc = []
+    cv_mcc = []
+    cv_iba = []
     
     # get the gt and pred for a single cv
     for cv_predl, cv_predp, cv_gt in zip(config_predl, 
                                          config_predp,
                                          config_gt):
 
-        # Compute the sensitivity and specificity for each round
-        sens, spec = LabelsToSensitivitySpecificity(cv_gt, cv_predl)
+        # Compute the different metrics for each round of cv
+        sens, spec = LabelsToSensitivitySpecificity(cv_gt, cv_predl) 
+        prec, npv = LabelsToPrecisionNegativePredictiveValue(cv_gt, cv_predl)
+        gmean = LabelsToGeometricMean(cv_gt, cv_predl)
+        acc = LabelsToAccuracy(cv_gt, cv_predl)
+        f1sc = LabelsToF1score(cv_gt, cv_predl)
+        mcc = LabelsToMatthewCorrCoef(cv_gt, cv_predl)
+        iba = LabelsToGeneralizedIndexBalancedAccuracy(cv_gt, cv_predl)
+
         cv_sens.append(sens)
         cv_spec.append(spec)
+        cv_prec.append(prec)
+        cv_npv.append(npv)
+        cv_gmean.append(gmean)
+        cv_acc.append(acc)
+        cv_f1sc.append(f1sc)
+        cv_mcc.append(mcc)
+        cv_iba.append(iba)
 
     # Compute the mean-std of the different cvs
+    ### mean
     mean_sens_config.append(np.mean(cv_sens))
     mean_spec_config.append(np.mean(cv_spec))
+    mean_prec_config.append(np.mean(cv_prec))
+    mean_npv_config.append(np.mean(cv_npv))
+    mean_gmean_config.append(np.mean(cv_gmean))
+    mean_acc_config.append(np.mean(cv_acc))
+    mean_f1sc_config.append(np.mean(cv_f1sc))
+    mean_mcc_config.append(np.mean(cv_mcc))
+    mean_iba_config.append(np.mean(cv_iba))
+
+    ### std
     std_sens_config.append(np.std(cv_sens))
     std_spec_config.append(np.std(cv_spec))
+    std_prec_config.append(np.std(cv_prec))
+    std_npv_config.append(np.std(cv_npv))
+    std_gmean_config.append(np.std(cv_gmean))
+    std_acc_config.append(np.std(cv_acc))
+    std_f1sc_config.append(np.std(cv_f1sc))
+    std_mcc_config.append(np.std(cv_mcc))
+    std_iba_config.append(np.std(cv_iba))
